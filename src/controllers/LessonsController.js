ce3a74b2e5c582120c1d7ba11cfdd99c498e3193
obrunofontana <obrunofontana@gmail.com>
2022-07-20 19:17:58 -0300

    feat: 20-07: implementa busca de aula por id

--- src/controllers/LessonsController.js
+++ src/controllers/LessonsController.js
@@ -78,3 +78,44 @@ exports.create = async (req, res) => {
     return res.status(500).send({ error: e.message || e });
   }
 }
+
+exports.findById = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const lesson = await knex
+      .select('*')
+      .from('lessons')
+      .where({ id })
+      .first();
+
+    if (!lesson) {
+      return res.status(404).send({
+        status: `Nenhuma aula com o id: ${id} foi encontrada`
+      });
+    }
+
+    const instructor = await knex
+      .select('*')
+      .from('instructors')
+      .where({ id: lesson.instructorId })
+      .first();
+
+    delete lesson.instructorId;
+    delete lesson.courseId;
+
+    delete instructor.id;
+
+    // define um avatar padrão se não existir
+    if (!instructor.avatarUrl) {
+      instructor.avatarUrl = 'https://avatars.dicebear.com/api/big-smile/your-custom-seed.svg';
+    }
+
+    return res.status(200).send({
+      ...lesson,
+      instructor
+    });
+  } catch (e) {
+    return res.status(500).send({ error: e.message || e });
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -18,6 +18,7 @@ routes.get('/courses/:id', CoursesController.findById);
 routes.post('/instructors', InstructorsController.create);
 
 routes.post('/lessons', LessonsController.create);
+routes.get('/lessons/:id', LessonsController.findById);
 
 module.exports = routes;
 
